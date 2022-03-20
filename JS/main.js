73b826d42615c18e97d3e7f55082793baafc69ec
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-03-20 22:54:11 +0300

    create order add function

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,14 @@
-.
+//
+$("#place-order").click(function () {
+        let flavour = $(".flavour option:selected").val();
+        let size = $("#size option:selected").val();
+        let crust = $("#crust option:selected").val();
+        let topping = $("#toppings option:selected").val();
+        let number = $("#number").val();
+        console.log(size);
+
+//Function order
+        let order = (flavour, size, crust, topping, number, total) => {
+            return {flavour, size, crust, topping, number, total};
+
+//Compute order price
