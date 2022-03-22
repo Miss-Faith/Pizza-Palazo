fa5be394fe5e32278cb44b7742c5810ba3ea9d1e
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-03-22 11:58:19 +0300

    Update checkout button and styling

--- JS/main.js
+++ JS/main.js
@@ -11,8 +11,6 @@ function Pizzaorder(type, size, crust, topping, total) {
 }
 $(document).ready(function() {
 
-  $("button#add-pizza").click(function(event) {
-    event.preventDefault();
     let pizzaType = $(".pizza-type").val();
     let pizzaSize = $(".pizza-size").val();
     let pizzaCrust = $(".pizza-crust").val();
@@ -21,6 +19,12 @@ $(document).ready(function() {
        pizzaTopping.push($(this).val());
   });
   pizzaTopping.join(", ");
+
+  let deliveryFee = 150;
+  let clientName = $("#client-name").val();
+  let clientNumber = $("#client-number").val();
+  let clientLocation = $("#client-location").val();
+
 //Compute order price
   switch (pizzaSize) {
     case "Large":
@@ -52,9 +56,14 @@ $(document).ready(function() {
   let toppingPrice = pizzaTopping.length * toppingValue;
 
   totalPrice = sizePrice + crustPrice + toppingPrice;
+  let checkoutTotal =0;
+  checkoutTotal = checkoutTotal + totalPrice;
+  checkoutTotalWithDelivery = checkoutTotal + deliveryFee;
 
   var newOrder = new Pizzaorder(pizzaType, pizzaSize, pizzaCrust, pizzaTopping, totalPrice);
 
+  $("button#add-pizza").click(function(event) {
+    event.preventDefault();
     $("#description").hide();
     $("#order-details").show();
     $("#orders-made").append(
@@ -69,38 +78,27 @@ $(document).ready(function() {
 
     $("#delivery").change(function() {
         if ($(this).prop('checked')) {
-            alert("We charge an additional Ksh.200 for deliveries.");
-            $("#order-details").hide();
-            $("#location").show();
+          alert("We charge an additional Ksh.150 for deliveries.");
+          return false;
         }
         else {
             return false;
         }
     });
-    $("button#add-location").click(function(event) {
-      event.preventDefault();
-      let deliveryFee = 150;
-      let clientName = $(".client-name").val();
-      let clientNumber = $(".client-number").val();
-      let clientLocation = $(".client-location").val();
-
-      $("#location").hide();
-      $("#description").hide();
-      $("#order-details").show();
-    });
 
     $("button#checkout").click(function(event) {
       event.preventDefault();
-      $("#orders-made").append(
-        '<tr><td id="pizzatype">'+newOrder.type +
-        '</td><td id="pizzasize">' + newOrder.size +
-        '</td><td id="pizzacrust">'+newOrder.crust +
-        '</td><td id="pizzatopping">'+newOrder.topping+
-        '</td><td id="total">'+newOrder.total+
-        '</td></tr>');
-
-
-      //$("#location").hide();
-      //$("#order-details").show();
+      $("button#add-pizza").hide()
+      $("button#checkout").hide()
+      $("fieldset").hide()
+        if ($("#delivery").prop('checked')) {
+          $("#location").show();
+          $("button#add-location").click(function(event) {
+             event.preventDefault();
+             $("#final-message").append(`Hi + ${clientName} +  your total order is Ksh.${checkoutTotalWithDelivery} +. Delivery will be made to  + ${clientLocation} +  in 30 mins.`);
+             return false;
+           });
+        }else
+          $("#final-message").append(`Hi, your total order is Ksh.${checkoutTotal} . Your order will be ready for pick up in 30 mins.`);
     });
 });
